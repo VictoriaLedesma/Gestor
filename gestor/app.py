3090c1f87763457bcaeddf8f2734f9a7b267579b
Federico Reiz <federico.reiz@davinci.edu.ar>
2024-11-17 16:23:25 -0300

    Solución error e hice el frontend forms para "Agregar gastos" y "Editar ingresos"
    Hice el frontend para los buttons de Agregar gasto y Editar ingresos, arreglé un error que había el cual no permitía la redirección del inicio a ingresos al clickear en "ver todo" y corregí detalles del css en inicio

--- gestor/app.py
+++ gestor/app.py
@@ -10,6 +10,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/gestor'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'clave_secreta'
 
+
+@app.route('/ingresos')
+def ingresos():
+    if 'usuario' in session:
+        return render_template('ingresos.html', usuario=session['usuario'])
+    return redirect(url_for('login'))
+
+
+
 @app.route('/inicio')
 def inicio():
     if 'usuario' in session:
@@ -154,3 +163,6 @@ def home():
     return render_template('index.html', data=data)
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# AGREGAR GASTOS
